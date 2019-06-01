4cb5a53ed8eda5821075a7a4e5edc9c2c3917b51
waynes <way@zhangdeMacBook-Air.local>
2019-06-01 11:15:23 +0800

    completed lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,8 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int n = 0;
+        for(int i = 0; i < 10; i += 1) {
+            System.out.println(n = n + i);
+        }
+    }
+}
